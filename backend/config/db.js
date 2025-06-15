c61e544f222b295649e3c1b4b699f3a1eeab23b5
NesrinNoufal <nesrinnoufal99@gmail.com>
2025-06-15 23:14:34 +0530

    logs removed

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ import mongoose from 'mongoose';
 const connectToDb = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/video-conferencing');
-    console.log('Connected to MongoDB');
+    // console.log('Connected to MongoDB');
   } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
+    // console.error('Failed to connect to MongoDB:', error);
   }
 };
 
